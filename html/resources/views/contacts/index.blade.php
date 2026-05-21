dcad5a20aca6c1a925e1139a6ecbeb2454b5a76d
Raylla <raylla_op12@hotmail.com>
2026-05-21 20:18:07 -0300

    feat: add session flash messages and update links

--- html/resources/views/contacts/index.blade.php
+++ html/resources/views/contacts/index.blade.php
@@ -3,16 +3,28 @@
 @section('title', 'List of Contacts')
 
 @section('content')
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
     <table class="table">
         <thead>
             <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Contact</th>
-            <th scope="col">Email</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-            <th scope="col">View</th>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Contact</th>
+                <th scope="col">Email</th>
+                <th scope="col">Edit</th>
+                <th scope="col">View</th>
+                <th scope="col">Delete</th>
             </tr>
         </thead>
         <tbody>
@@ -22,9 +34,17 @@
                     <td>{{$contact->name}}</td>
                     <td>{{$contact->contact}}</td>
                     <td>{{$contact->email}}</td>
-                    <td><a href="#"><i class="fa-solid fa-pen"></i></a></td>
-                    <td><a href="#"><i class="fa-solid fa-trash"></i></a></td>
-                    <td><a href="#"><i class="fa-solid fa-eye"></i></a></td>
+                    <td><a href="{{ route('contacts.show', $contact->id) }}"><i class="fa-solid fa-eye"></i></a></td>
+                    <td><a href="{{ route('contacts.edit', $contact->id) }}"><i class="fa-solid fa-pen"></i></a></td>
+                    <td>
+                        <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>

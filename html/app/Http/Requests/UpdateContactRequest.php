d835a17bbe0b5e7cb8d411d46641e4b633bbbadf
Raylla <raylla_op12@hotmail.com>
2026-05-21 20:17:22 -0300

    feat: create update contact request

---
+++ html/app/Http/Requests/UpdateContactRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $id = $this->route('contact')->id;
+        return [
+            'name' => 'required|string|min:5|max:255',
+            'email' => 'required|email|unique:contacts,email,' . $id,
+            'contact' => 'required|digits:9|unique:contacts,contact,' . $id,
+        ];
+    }
+}

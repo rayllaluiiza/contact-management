69baca01366ee10253c9706c2b133f85584f5609
Raylla <raylla_op12@hotmail.com>
2026-05-21 21:25:09 -0300

    feat: add user seeder

--- html/database/seeders/DatabaseSeeder.php
+++ html/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Contact::factory()->count(10)->create();
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('123456')
+        ]);
     }
 }
